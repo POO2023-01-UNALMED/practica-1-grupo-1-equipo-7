738eceb94adf798e6e32d0ad3fa1f2acd057a0d3
Daimon-22 <elvelandiar@unal.edu.co>
2023-05-21 21:17:32 -0500

    gestionHospital y descripcion

---
+++ src/uiMain/Gestion/gestionHospital/VerHabitacion.java
@@ -0,0 +1,28 @@
+package uiMain.Gestion.gestionHospital;
+
+import gestorAplicacion.administracion.Hospital;
+import gestorAplicacion.administracion.CategoriaHabitacion;
+import gestorAplicacion.servicios.Habitacion;
+
+import java.util.Scanner;
+
+public class VerHabitacion {
+    static Scanner sc = new Scanner(System.in);
+    public static void verHabitacion (Hospital hospital){
+        System.out.println("Ingrese el tipo de habitacion que desea ver: ");
+        System.out.println("'CAMILLA', 'INDIVIDUAL', 'DOBLE', 'OBSERVACION', 'UCI', 'UCC'");
+        String categoriaSeleccionada = sc.next();
+        CategoriaHabitacion habitacionSeleccionada = CategoriaHabitacion.valueOf(categoriaSeleccionada);
+        for (Habitacion habitacion: hospital.habitaciones){
+            if (habitacion.getCategoria()==habitacionSeleccionada){
+                String ocupada="Libre";
+                if(habitacion.isOcupada()){
+                    ocupada="Ocupada";
+                }
+
+                System.out.println("Numero ID de la habitación: "+ habitacion.getNumero()+ "Tipo de habitación: "+ ocupada);
+            }
+        }
+    }
+
+}
