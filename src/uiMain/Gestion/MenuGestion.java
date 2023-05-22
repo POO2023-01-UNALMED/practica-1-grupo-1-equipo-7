200cf659b9b919ec48bf2896e91e6739184e3e94
DiegoGG512 <124928460+DiegoGG512@users.noreply.github.com>
2023-05-21 21:31:26 -0500

    Se añade el menu de gestion doctor

--- src/uiMain/Gestion/MenuGestion.java
+++ src/uiMain/Gestion/MenuGestion.java
@@ -2,6 +2,7 @@ package uiMain.Gestion;
 
 import baseDatos.Serializador;
 import gestorAplicacion.administracion.Hospital;
+import uiMain.Gestion.gestionDoctores.GestionDoctor;
 import uiMain.Gestion.gestionPacientes.GestionPaciente;
 import uiMain.Gestion.gestionVacunas.GestionVacunas;
 
@@ -17,6 +18,7 @@ public class MenuGestion {
             System.out.println("\nMENU Gestion");
             System.out.println("1. Gestionar Pacientes");
             System.out.println("2. Gestionar apartado de vacunas");
+            System.out.println("3. Gestionar Doctores");
             System.out.println("6. Regresar al menu inicial");
             System.out.println("7. Salir");
             System.out.println("Ingrese una opcion: ");
@@ -25,6 +27,7 @@ public class MenuGestion {
             switch (opcion) {
                 case 1 -> GestionPaciente.menuGestionPaciente(hospital);
                 case 2 -> GestionVacunas.menuGestionVacunas(hospital);
+                case 3 -> GestionDoctor.menuGestionDoctor(hospital);
                 case 6 -> {
                     return;
                 }
---
+++ src/uiMain/Gestion/gestionDoctores/GestionDoctor.java
@@ -0,0 +1,37 @@
+package uiMain.Gestion.gestionDoctores;
+
+import baseDatos.Serializador;
+import gestorAplicacion.administracion.Hospital;
+
+import java.util.Scanner;
+
+public class GestionDoctor {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void menuGestionDoctor(Hospital hospital) {
+        byte opcion;
+        do {
+            System.out.println("\nMENU Gestion Doctor");
+            System.out.println("1. Registrar doctor");
+            System.out.println("2. Eliminar doctor");
+            System.out.println("3. Ver doctor");
+            System.out.println("4. Añadir citas");
+            System.out.println("5. Eliminar citas");
+            System.out.println("6. Regresar al menu anterior");
+            System.out.println("7. Salir");
+            System.out.println("Ingrese una opcion: ");
+            opcion = sc.nextByte();
+            switch (opcion) {
+                case 1 -> RegistrarDoctor.registrarDoctor(hospital);
+                case 3 -> VerDoctor.verDoctor(hospital);
+                case 6 -> {
+                    return;
+                }
+                case 7 -> {
+                    Serializador.serializar(hospital);
+                    System.exit(0);
+                }
+            }
+        } while (true);
+    }
+}
