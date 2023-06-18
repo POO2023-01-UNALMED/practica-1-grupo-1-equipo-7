5d5876b5dbf11d8c0a41159468843d1269f0ed24
Juan Camilo Gutiérrez Martínez <juagutierrezma@unal.edu.co>
2023-06-18 13:05:31 -0500

    Se agrega ver vacunas hospital con text.
    Co-authored-by: saarboledaa <saarboledaa@unal.edu.co>
    Co-authored-by: dangv31 <dangiraldova@unal.edu.co>
    Co-authored-by: Daimon-22 <elvelandiar@unal.edu.co>
    Co-authored-by: DiegoGG512 <dgraciag@unal.edu.co>

--- Python/src/ui_main/gestion/gestion_hospital/ver_vacunas.py
+++ Python/src/ui_main/gestion/gestion_hospital/ver_vacunas.py
@@ -1,2 +1,39 @@
-def ver_vacunas():
-    pass
+import tkinter as tk
+
+def imprimir_titulo(frame_implementacion):
+    # Limpia el frame
+    for item in frame_implementacion.winfo_children():
+        item.destroy()
+
+    # Imprime el titulo
+    titulo = tk.Label(frame_implementacion, text="Ver vacunas del hospital", bg="white", font=("Helvetica", 16, "bold"))
+    titulo.pack(pady=20)
+
+def ver_vacunas(hospital,frame_implementacion):
+    imprimir_titulo(frame_implementacion)
+
+    titulo_ver_vacunas = tk.Label(frame_implementacion, text="Estas son todas las vacunas presentes en el hospital (Recuerda si es necesario, desplazarse hacia abajo)",
+    bg="white",font=("Helvetica", 10, "bold"))
+    titulo_ver_vacunas.pack(pady=10)
+
+    vacunas_text = tk.Text(frame_implementacion, bg="white", font=("Helvetica", 14))
+    vacunas_text.pack(fill=tk.BOTH,expand=True)
+
+    # Mostrar todas las vacunas presentes en el hospital
+    for vacuna in hospital.lista_vacunas:
+        nombre = vacuna.nombre
+        tipo = vacuna.tipo
+        tipo_eps =",".join(vacuna.tipo_eps)
+        fechas=[]
+        for cita in vacuna.agenda:
+            fechas.append(cita.fecha)
+        agenda = ",".join(fechas)
+        precio = vacuna.precio
+
+        texto_vacuna = f"Nombre: {nombre}\nTipo: {tipo}\nPrecio: {precio}\nEps a la que pertenece: {tipo_eps}\nAgenda: {agenda}\n\n"
+
+        # Insertar el texto de la vacuna en el widget Text
+        vacunas_text.insert(tk.END, texto_vacuna)
+
+        vacunas_text.config(padx=30)
+        vacunas_text.config(highlightthickness=5, highlightbackground="blue")
