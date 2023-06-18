68bd5b5726e6c53251064b57fe58082d983bf708
dangv31 <dangiraldova@unal.edu.co>
2023-06-18 13:22:45 -0500

    Formula medica mas bonita
    Co-Authored-By: Daimon-22 <124916480+Daimon-22@users.noreply.github.com>
    Co-Authored-By: Juan Camilo Gutiérrez Martínez <97555202+Camilog2004@users.noreply.github.com>
    Co-Authored-By: DiegoGG512 <124928460+DiegoGG512@users.noreply.github.com>
    Co-Authored-By: Santiago Arboleda <124930965+saarboledaa@users.noreply.github.com>

--- Python/src/ui_main/funcionalidades/formula_medica.py
+++ Python/src/ui_main/funcionalidades/formula_medica.py
@@ -10,7 +10,7 @@ def imprimir_titulo(frame):
         item.destroy()
 
     # Imprime el titulo
-    titulo = tk.Label(frame, text="Formular Medicamentos", bg="white")
+    titulo = tk.Label(frame, text="Formular Medicamentos", bg="white", font=("Helvetica", 10, "bold"))
     titulo.pack()
 
 
@@ -32,7 +32,7 @@ def formula_medica(hospital, frame):
 
         imprimir_titulo(frame)  # Imprime el titulo de "Formular medicamentos"
         # Label que muestra la informacion del paciente en la parte superior
-        info_paciente = tk.Label(frame, text=f"{paciente.nombre} - CC: {paciente.cedula}", bg="white")
+        info_paciente = tk.Label(frame, text=f"{paciente.nombre} - CC: {paciente.cedula}", bg="white", font=("Helvetica", 10, "bold"))
         info_paciente.pack()
 
         # Frame que va a contener todos los widgets de la funcionalidad
@@ -40,7 +40,7 @@ def formula_medica(hospital, frame):
         frame_datos.pack()
 
         # Label con instruccion de seleccionar enfermedad a tratar y combobox de enfermedades
-        enfermedad_tratar = tk.Label(frame_datos, text="Seleccione la enfermedad a tratar: ", bg="white")
+        enfermedad_tratar = tk.Label(frame_datos, text="Seleccione la enfermedad a tratar: ", bg="white", font=("Helvetica", 10, "bold"))
         enfermedad_tratar.grid(row=0, column=0, padx=10, pady=10, sticky="w")
         valor_defecto1 = tk.StringVar
         combo_enfermedades = ttk.Combobox(frame_datos, values=paciente.historia_clinica.enfermedades,
@@ -49,7 +49,7 @@ def formula_medica(hospital, frame):
         combo_enfermedades.grid(row=0, column=1, padx=10, pady=10, sticky="w")
 
         # Label con instruccion de seleccionar doctor que formulara y combobox de doctores
-        elegir_doctor = tk.Label(frame_datos, text="Seleccione el doctor que hará la formula:", bg="white")
+        elegir_doctor = tk.Label(frame_datos, text="Seleccione el doctor que hará la formula:", bg="white", font=("Helvetica", 10, "bold"))
         elegir_doctor.grid(row=1, column=0, padx=10, pady=10, sticky="w")
         valor_defecto2 = tk.StringVar
         combo_elegir_doctor = ttk.Combobox(frame_datos, textvariable=valor_defecto2, state="disabled")
@@ -71,7 +71,7 @@ def formula_medica(hospital, frame):
         medicamentos_disponibles = paciente.med_enfermedad(enfermedad, hospital)
 
         # Título de la selección de medicamentos
-        titulo_seleccion = tk.Label(frame_seleccion, text="Seleccione los medicamentos:", bg="white")
+        titulo_seleccion = tk.Label(frame_seleccion, text="Seleccione los medicamentos:", bg="white", font=("Helvetica", 10, "bold"))
         titulo_seleccion.pack()
 
         # Listbox para la selección de medicamentos
@@ -97,7 +97,7 @@ def formula_medica(hospital, frame):
         medicamentos_seleccionados = [disponibles[indice] for indice in indices_seleccionados]
         frame_meds_seleccionados = tk.Frame(frame)
         frame_meds_seleccionados.pack(fill="both", expand=True)
-        label_meds_seleccionados = tk.Label(frame_meds_seleccionados, text="Estos son sus medicamentos", bg="white")
+        label_meds_seleccionados = tk.Label(frame_meds_seleccionados, text="Estos son sus medicamentos", bg="white", font=("Helvetica", 10, "bold"))
         label_meds_seleccionados.pack()
         formula.lista_meds = medicamentos_seleccionados
         for med in medicamentos_seleccionados:
@@ -123,7 +123,7 @@ def formula_medica(hospital, frame):
         imprimir_titulo(frame)
         frame_formulas = tk.Frame(frame)
         frame_formulas.pack(fill="both", expand=True)
-        label = tk.Label(frame_formulas, text="Historial de formulas", bg="white")
+        label = tk.Label(frame_formulas, text="Historial de formulas", bg="white", font=("Helvetica", 10, "bold"))
         label.pack(fill="both")
         for formula in paciente.historia_clinica.lista_formulas:
             formula_label = tk.Label(frame_formulas, text=formula)
@@ -152,7 +152,7 @@ def formula_medica(hospital, frame):
     imprimir_titulo(frame)
 
     # Pide la cedula del paciente
-    label_ingreso_cedula = tk.Label(frame, text="Ingrese la cédula del paciente:", bg="white")
+    label_ingreso_cedula = tk.Label(frame, text="Ingrese la cédula del paciente:", bg="white", font=("Helvetica", 10, "bold"))
     label_ingreso_cedula.pack()
 
     criterios = ["Cédula"]
